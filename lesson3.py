1485de2e5ed5562dba00d100d2c212e395dc9e24
Anton Galustov <galustoffanton@yandex.ru>
2022-09-07 19:20:22 +0300

    'initial3'

--- lesson3.py
+++ lesson3.py
@@ -1,2 +1,11 @@
 print('Hello world')
-print('Be Happy')
+summ = 0
+while True:
+    n = int(input('введите число '))
+    num = n % 10 
+    if num == 0:
+      summ += n
+print('число',summ)
+
+    
+
